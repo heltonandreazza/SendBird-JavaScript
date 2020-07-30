6e7fa2a6a9c98f588c2405ce589e061f9d2f1c53
Helton Andreazza <heltonandreazza@Heltons-MacBook-Air.local>
2020-07-30 09:10:03 -0300

    remove redux and add react hooks to Start screen

--- react-native-redux-sample/ReactNativeWithSendBird/src/screens/Start.js
+++ react-native-redux-sample/ReactNativeWithSendBird/src/screens/Start.js
@@ -1,70 +1,54 @@
-import React, { Component } from 'react';
-import { View } from 'react-native';
-import AsyncStorage from '@react-native-community/async-storage';
-import { connect } from 'react-redux';
-import { NavigationActions, StackActions } from 'react-navigation';
-import { sbConnect } from '../sendbirdActions';
-import { Spinner } from '../components';
+import React, { useState, useLayoutEffect } from 'react'
+import { View } from 'react-native'
+import AsyncStorage from '@react-native-community/async-storage'
+import { NavigationActions, StackActions } from 'react-navigation'
+import { sbConnect } from '../sendbirdActions'
+import { Spinner } from '../components'
 
-class Start extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isLoading: false
-    };
-  }
+const Start = ({ navigation }) => {
+  const [isLoading, setIsLoading] = useState(false)
 
-  componentDidMount() {
+  useLayoutEffect(() => {
+    function redirectTo(page, params) {
+      navigation.dispatch(
+        StackActions.reset({
+          index: 0,
+          actions: [NavigationActions.navigate({ routeName: page, params })],
+        }),
+      )
+    }
+
     AsyncStorage.getItem('user', (err, result) => {
       if (result) {
-        this.setState({ isLoading: true }, () => {
-          const user = JSON.parse(result);
-          sbConnect(user.userId, user.nickname)
-            .then(() => {
-              this.setState({ isLoading: false }, () => {
-                this.redirectTo('Menu');
-              });
-            })
-            .catch(err => {
-              this.setState({ isLoading: false }, () => {
-                this.redirectTo('Login');
-              });
-            });
-        });
-      } else this.redirectTo('Login');
-    });
-  }
-  redirectTo(page, params) {
-    console.info('redirectTo', page, params)
-    this.props.navigation.dispatch(
-      StackActions.reset({
-        index: 0,
-        actions: [NavigationActions.navigate({ routeName: page, params: params })]
-      })
-    );
-  }
-  render() {
-    return (
-      <View style={styles.containerStyle}>
-        <Spinner visible={this.state.isLoading} />
-      </View>
-    );
-  }
+        setIsLoading(true)
+        const user = JSON.parse(result)
+        sbConnect(user.userId, user.nickname)
+          .then(() => {
+            setIsLoading(false)
+            redirectTo('Menu')
+          })
+          .catch(() => {
+            setIsLoading(false)
+            redirectTo('Login')
+          })
+      } else {
+        redirectTo('Login')
+      }
+    })
+  }, [navigation])
+
+  return (
+    <View style={styles.containerStyle}>
+      <Spinner visible={isLoading} />
+    </View>
+  )
 }
 
-function mapStateToProps({ login }) {
-  const { error, user } = login;
-  return { error, user };
-}
-
-export default connect(
-  mapStateToProps,
-  {}
-)(Start);
+export default Start
 
 const styles = {
   containerStyle: {
     backgroundColor: '#fff',
-    flex: 1
-  }
-};
+    flex: 1,
+  },
+}
